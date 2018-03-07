9bd0602969eaa526b94c987d8586f337d549497b
Jon Cockayne <benorn@gmail.com>
2018-03-07 09:30:11 +0000

    Hooked up collocation matrices to python

--- python/eit/collocate.pyx
+++ python/eit/collocate.pyx
@@ -8,7 +8,19 @@ cdef extern from "collocate.hpp":
 		MatrixXd mu_mult
 		MatrixXd cov
 		CollocationResult(MatrixXd mu_mult, MatrixXd cov)
+	cdef cppclass CollocationMatrices:
+		MatrixXd kern;
+		MatrixXd left;
+		MatrixXd central;
+		CollocationMatrices(MatrixXd kern, MatrixXd left, MatrixXd central)
 	cdef unique_ptr[CollocationResult] _collocate_no_obs "collocate_no_obs"(
+		Map[MatrixXd] x,
+		Map[MatrixXd] interior,
+		Map[MatrixXd] boundary,
+		Map[MatrixXd] sensors,
+		Map[VectorXd] kernel_args
+	)
+	cdef unique_ptr[CollocationMatrices] _collocate_matrices_no_obs "collocate_matrices_no_obs"(
 		Map[MatrixXd] x,
 		Map[MatrixXd] interior,
 		Map[MatrixXd] boundary,
@@ -65,6 +77,25 @@ def collocate_no_obs(
 	mu_mult = ndarray_copy(deref(ret).mu_mult)
 	cov = ndarray_copy(deref(ret).cov)
 	return mu_mult, cov
+
+def collocate_matrices_no_obs(
+	np.ndarray[dtype=np.float_t, ndim=2] x,
+	np.ndarray[dtype=np.float_t, ndim=2] interior,
+	np.ndarray[dtype=np.float_t, ndim=2] boundary,
+	np.ndarray[dtype=np.float_t, ndim=2] sensors,
+	np.ndarray[dtype=np.float_t, ndim=1] kernel_args
+):
+	cdef unique_ptr[CollocationMatrices] ret = _collocate_matrices_no_obs(
+		Map[MatrixXd](x),
+		Map[MatrixXd](interior),
+		Map[MatrixXd](boundary),
+		Map[MatrixXd](sensors),
+		Map[VectorXd](kernel_args)
+	)
+	kern = ndarray_copy(deref(ret).kern)
+	left = ndarray_copy(deref(ret).left)
+	central = ndarray_copy(deref(ret).central)
+	return kern, left, central
 
 def log_likelihood(
 	np.ndarray[dtype=np.float_t, ndim=2] interior,
